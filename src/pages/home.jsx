db6216317a53bd2de6cc6a9426a785f01d06c5d7
higo jhon <higojhon98@gmail.com>
2023-11-17 19:03:18 -0300

    FEAT: Adcionando img do bolo,  inforaçõe e CSS

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Footer from '../components/footer';
 import '../style/home.css';
 import Paes from '../assets/paes.jpeg';
 import Pao from '../assets/pao.jpeg';
+import bolo from '../assets/bolo.jpeg';
 
 const Home = () => {
     return (
@@ -15,18 +16,26 @@ const Home = () => {
                     <img src={Paes} alt="Pão delicioso"/>
                 </div>
                 <br />
-                <div className='inf-pao'>
-                    <img src={Pao} alt="informações do pao" />
-                    <div>
-                        <h3>Pão Frances</h3>
-                        <p>Deliciosos pães frescos feitos diariamente na nossa padaria. Experimente a qualidade e sabor incomparáveis!</p>
-                        <p>1$ por unidade.</p>
+                <div className='div-itens'>
+                    <div className='div-inf'>
+                        <img src={Pao} alt="informações do pao" />
+                        <div>
+                            <h3>Pão Frances</h3>
+                            <p>Deliciosos pães frescos feitos diariamente na nossa padaria. Experimente a qualidade e sabor incomparáveis!</p>
+                            <p>1$ por unidade.</p>
+                        </div>
+                    </div>
+                    <br />
+                    <div className='div-inf'>
+                        <img src={bolo} alt="informações do bolo" />
+                        <div>
+                            <h3>Bolo de Chocolate</h3>
+                            <p>Venha conhecer nossos bolos e tortas, ouvir dizer por ai que é o melhor da região!</p>
+                            <p>10$ por unidade.</p>
+                        </div>
                     </div>
                 </div>
-                    <h1>Home</h1>
-                    <h1>Home</h1>
-                    <h1>Home</h1>
-                    <h1>Home</h1>
+                <br />
             </main>
             <footer>
                 <Footer />
