fb737d3ab84b00e50281b69e7151518121475197
thiagohcc <dzno23@gmail.com>
2023-11-16 21:34:41 -0300

    FEAT: Aprimora o header para mobile e adiciona o botão de encomendas

--- src/components/hearder.jsx
+++ src/components/hearder.jsx
@@ -1,20 +1,67 @@
+import { useState } from 'react';
 import '../style/header.css';
 import logo from '../assets/logo.svg';
+import menuBtn from '../assets/menu-btn.png';
 
-const header = () => {
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toogleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        console.log('clicou');
+    };
+
+    const handleClickMenu = () => {
+        setTimeout(() => {
+            setIsMenuOpen(!isMenuOpen);
+        }, 200);
+    };
+
     return (
         <header>
-            <div>
-                <img className="logo-img" src={logo} alt="logo" />
+            <div className='menu mobile'>
+                <div>
+                    <img className="logo-img" src={logo} alt="logo" />
+                </div>
+                <img className='menu-btn' src={menuBtn} onClick={toogleMenu} alt="menu" />
             </div>
-            <div className='menu'>
-                <span className='item-menu'>PÁGINA INICIAL</span>
-                <span className='item-menu'>QUEM SOMOS</span>
-                <span className='item-menu'>PRODUTOS</span>
-                <span className='item-menu'>FALE CONOSCO</span>
+            <div className={`menu ${isMenuOpen ? 'active' : ''}`}>
+                <span
+                    className='item-menu'
+                    onClick={handleClickMenu}
+                >
+                    PÁGINA INICIAL
+                </span>
+                <span
+                    className='item-menu'
+                    onClick={handleClickMenu}
+                >
+                    QUEM SOMOS
+                </span>
+                <span
+                    className='item-menu'
+                    onClick={handleClickMenu}
+                >
+                    UNIDADES
+                </span>
+                <span
+                    className='item-menu'
+                    onClick={handleClickMenu}
+                >
+                    PRODUTOS
+                </span>
+                <span
+                    className='item-menu'
+                    onClick={handleClickMenu}
+                >
+                    FALE CONOSCO
+                </span>
+            </div>
+            <div className='encomendas-btn'>
+                <span>ENCOMENDAS</span>
             </div>
         </header>
     );
 }   
 
-export default header;
+export default Header;
