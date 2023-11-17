865f5713afe42f9bc596e03fa34c440faf0d46ce
higo jhon <higojhon98@gmail.com>
2023-11-17 18:41:40 -0300

    FEAT: Adcionando img do pao,  inforaçõe e CSS

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,8 @@
 import Hearder from '../components/hearder';
 import Footer from '../components/footer';
-import Paes from '../assets/paes.jpeg';
 import '../style/home.css';
+import Paes from '../assets/paes.jpeg';
+import Pao from '../assets/pao.jpeg';
 
 const Home = () => {
     return (
@@ -12,6 +13,15 @@ const Home = () => {
             <main>
                 <div className='img-paes'>
                     <img src={Paes} alt="Pão delicioso"/>
+                </div>
+                <br />
+                <div className='inf-pao'>
+                    <img src={Pao} alt="informações do pao" />
+                    <div>
+                        <h3>Pão Frances</h3>
+                        <p>Deliciosos pães frescos feitos diariamente na nossa padaria. Experimente a qualidade e sabor incomparáveis!</p>
+                        <p>1$ por unidade.</p>
+                    </div>
                 </div>
                     <h1>Home</h1>
                     <h1>Home</h1>
