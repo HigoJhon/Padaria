c7c44f70b80d921123399d1a9ded7e70760fb201
higo jhon <higojhon98@gmail.com>
2023-11-17 18:14:32 -0300

    FEAT: Implementando a pagina de home e CSS

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,26 @@
 import Hearder from '../components/hearder';
 import Footer from '../components/footer';
+import Paes from '../assets/paes.jpeg';
+import '../style/home.css';
 
 const Home = () => {
     return (
         <div>
-            { <Hearder /> }
+            <header>
+                <Hearder />
+            </header>
             <main>
-                <h1>Home</h1>
+                <div className='img-paes'>
+                    <img src={Paes} alt="Pão delicioso"/>
+                </div>
+                    <h1>Home</h1>
             </main>
-            { <Footer /> }
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
+
 
 export default Home;
