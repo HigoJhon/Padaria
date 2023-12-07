c6973872170fd33f870f9cafb5ff2095c935f7ce
higo jhon <higojhon98@gmail.com>
2023-12-07 15:06:13 -0300

    Add Somos component and related styles

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,22 @@
 import Hearder from '../components/hearder';
 import Footer from '../components/footer';
 import Inicial from '../components/inicial';
+import Somos from '../components/somos';
 import '../style/home.css';
 import { useEffect, useState } from 'react';
 
 const Home = () => {
-    const [page, setPage] = useState('home');
+    const [page, setPage] = useState('somos');
 
     const handlePage = () => {
         if (page === 'home') {
             return (
                 <Inicial />
+            );
+        }
+        if (page === 'somos') {
+            return (
+                <Somos />
             );
         }
     };
